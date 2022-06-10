ffa02763ba1e896f469a553370b3a0174bb3d437
FacundoSalazar <facundev@outlook.com>
2022-06-09 21:13:12 -0300

    Agregado de fecha dinamica.

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -17,7 +17,7 @@ export default () => {
 
     const mainElements = document.createElement('section');
     mainElements.innerHTML = viewHome
-    
+
 
     /* Variables */
     const btnClickMain = mainElements.querySelector('.menu__btn') // Obtengo el div que hemos creado en el views/home.html
@@ -28,43 +28,49 @@ export default () => {
     const btnRegister = mainElements.querySelector('.main__button-register')
     const form = mainElements.querySelector('.none')
     const BtnformQuit = mainElements.querySelector('.form__quit')
+    const numeroNota = mainElements.querySelector('.form__numero-nota')
     const btnToday = mainElements.querySelector('.form__button-today')
+    const area = mainElements.querySelector('.form__area')
     const btnForms = mainElements.querySelector('.form__buttons') // Caja de los botones Limpiar y Agregar. Debo acceder a sus hijos.
     const boxDate = mainElements.querySelector('.form__date-box') // Caja del input y boton de fecha de entradas. Debo acceder a sus hijos.
-    const dateToday = Date();
-    console.log(dateToday)
+    const dateToday = new Date().toISOString().substring(0, 10); // Capturamos la fecha del día ycon substring le indicamos desde que indice
+    // hasta cual, queremos que obtener las fechas.
     /* Events Listener */
     if (homeWindow == "" || homeWindow == "#/") {
-        btnClickMain.addEventListener('click',() => {
+        btnClickMain.addEventListener('click', () => {
             main.classList.toggle('main__with-filters')
             aside.classList.remove('filters-off')
             aside.classList.add('filters')
             btnClickMain.classList.remove('menu__btn')
         })
-    
-        btnFilters.addEventListener('click', () =>{
+
+        btnFilters.addEventListener('click', () => {
             main.classList.remove('main__with-filters')
             aside.classList.add('filters-off')
             btnClickMain.classList.toggle('menu__btn')
         })
-        btnRegister.addEventListener('click', () =>{
+        btnRegister.addEventListener('click', () => {
             form.classList.remove('none')
             form.classList.add('form__container')
         })
-        BtnformQuit.addEventListener('click', () =>{
+        BtnformQuit.addEventListener('click', () => {
             form.classList.remove('form__container')
             form.classList.add('none')
         })
         btnToday.addEventListener('click', (e) => {
             e.preventDefault();
             if (boxDate.children[0].value == '') { // Terminar de hacer el ingreso de la fecha al input type=date.
-                alert()
-                boxDate.children[0].value == dateToday;
+                boxDate.children[0].value = dateToday;
             }
-            console.log(boxDate.children[0].value)
         })
-        
-    }
 
+        btnForms.children[0].addEventListener('click', (e) => {
+            e.preventDefault();
+            //dateToday.value = '';  No puedo aplicar value en un string.
+        })
+        /* btnForms.children[0].addEventListener('click', (e) => {
+            e.preventDefault();
+        }) */
+    }
     return mainElements
 }
