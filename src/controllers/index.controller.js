3164ae4a9c5e0e9d48c8161575cbbf9985f956e7
FacundoSalazar <facundev@outlook.com>
2022-07-14 10:46:07 -0300

    Agregando Fetch POST.

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -20,6 +20,7 @@ export default () => {
 
 
     /* Variables */
+    const url = 'http://localhost:8000/api/registers'
     const btnClickMain = mainElements.querySelector('.menu__btn') // Obtengo el div que hemos creado en el views/home.html
     const btnFilters = document.querySelector('.box__filters-menu') // Obtengo el boton de filtros que hemos creado en el index.html
     const aside = document.querySelector('aside') // Obtengo la etiqueta Aside que hemos creado en el index.html
@@ -135,14 +136,12 @@ export default () => {
             
             const myDataJson = JSON.stringify(datosForms)
             console.log(myDataJson);
-            
-            
-            fetch('http://localhost:3000/registers', {
-                method: 'Post',
-                body: myDataJson
-            })
 
             
+            fetch(url, {
+                method: 'post',
+                body: myDataJson
+            }).then(x => console.log('Datos enviados.'))
         })
     }
     return mainElements
--- src/index.js
+++ src/index.js
@@ -51,3 +51,4 @@ menuLogo.addEventListener('click', () => {
   document.location.href = "#"
 })
 
+
