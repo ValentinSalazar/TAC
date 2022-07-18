a63bfd29995a395e45f8179341f955cff207c126
FacundoSalazar <facundev@outlook.com>
2022-07-18 11:40:33 -0300

    Arreglando el envio de datos vacios.

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -118,15 +118,15 @@ export default () => {
             e.preventDefault();
             let nota = parseInt(numeroNota.children[1].value)
             let fecha = boxDate.children[0].value
-            let areaResponsable = firstSelect.innerText
+            let areaResponsable = firstSelect.value
             console.log(areaResponsable)
 
             let numero = secondSelect.value;
             let localidad = localidades[numero] 
 
-            let solicitanteForm = solicitante.innerText
+            let solicitanteForm = solicitante.value
             console.log(solicitanteForm)
-            let estadoForm = estado.innerText
+            let estadoForm = estado.value
             console.log(estadoForm)
 
             let datosForms = {
