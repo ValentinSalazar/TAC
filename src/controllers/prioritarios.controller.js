9bc8acb5dcb010e7e2ade1341965355dea3ae1da
Facundo Desktop <facundev@outlook.com>
2022-08-13 01:57:23 -0300

    Creando el apartado de Prioritarios y generando dos Fetchs. Un POST para enviar un registro y un GET dentro del controlador de prioritarios para obtener todos los registros relacionados a esa coleccion.

--- src/controllers/prioritarios.controller.js
+++ src/controllers/prioritarios.controller.js
@@ -1,86 +1,102 @@
-import viewPrioritarios from '../views/prioritarios.html'
+import viewPrioritarios from "../views/prioritarios.html";
 
 export default () => {
-    const url = 'http://localhost:8000/api/priorities'
-    const prioritariosElements = document.createElement('section')
-    prioritariosElements.innerHTML = viewPrioritarios;
-    const btnClickMain = prioritariosElements.querySelector('.menu__btn')
-    const btnFilters = document.querySelector('.box__filters-menu')
-    const aside = document.querySelector('aside')
-    const main = document.querySelector('main')
-    const prioritariosWindow = window.location.hash;
+  const url = "http://localhost:1337/api/priorities";
+  const prioritariosElements = document.createElement("section");
+  prioritariosElements.innerHTML = viewPrioritarios;
+  const btnClickMain = prioritariosElements.querySelector(".menu__btn");
+  const btnFilters = document.querySelector(".box__filters-menu");
+  const aside = document.querySelector("aside");
+  const main = document.querySelector("main");
+  const prioritariosWindow = window.location.hash;
+  const buttonPrioritarios = document.querySelector(".menu__pages").children[1].children[0];
 
-    const table = prioritariosElements.querySelector('.main__table')
-    table.style.marginTop = "4rem"
-    btnClickMain.style.marginTop = "-2rem"
+  const table = prioritariosElements.querySelector(".main__table");
+  table.style.marginTop = "4rem";
+  btnClickMain.style.marginTop = "-2rem";
 
+  if (prioritariosWindow == "#/Prioritarios") {
+    main.classList.remove("main__with-filters");
+    aside.classList.remove("filters");
+    aside.classList.add("filters-off");
+    
 
-    if (prioritariosWindow == "#/Prioritarios") {
-        main.classList.remove('main__with-filters')
-        aside.classList.remove('filters')
-        aside.classList.add('filters-off')
+    btnClickMain.addEventListener("click", () => {
+      main.classList.toggle("main__with-filters");
+      aside.classList.remove("filters-off");
+      aside.classList.add("filters");
+      btnClickMain.classList.remove("menu__btn");
+    });
 
-        btnClickMain.addEventListener('click', () => {
-            main.classList.toggle('main__with-filters')
-            aside.classList.remove('filters-off')
-            aside.classList.add('filters')
-            btnClickMain.classList.remove('menu__btn')
-        })
+    btnFilters.addEventListener("click", () => {
+      main.classList.remove("main__with-filters");
+      aside.classList.add("filters-off");
+      btnClickMain.classList.toggle("menu__btn");
+    });
 
-        btnFilters.addEventListener('click', () => {
-            main.classList.remove('main__with-filters')
-            aside.classList.add('filters-off')
-            btnClickMain.classList.toggle('menu__btn')
-        })
+    
+    // GET Method (Priorities Registers)
+    fetch(url)
+      .then((res) => res.json())
+      .then((prioritarios) => mostrarData(prioritarios))
+      .catch((err) => console.log(err));
 
+    const mostrarData = (data) => {
+      let body = `
+              <tr>
+                  <th>Nº</th>
+                  <th>Fecha de Entrada</th>
+                  <th>Area responsable</th>
+                  <th>Localidad</th>
+                  <th>Solicitante</th>
+                  <th>Estado</th>
+                  <th>Modificar</th>
+              </tr>
+          `;
+      for (let i = 0; i < data.length; i++) {
+        body += `
+               <tr>
+                  <td>${data[i].nota}</td>
+                  <td>${data[i].fecha}</td>
+                  <td>${data[i].areaResponsable}</td>
+                  <td>${data[i].localidad}</td>
+                  <td>${data[i].solicitanteForm}</td>
+                  <td>${data[i].estadoForm}</td>
+                  <td>
+                      <div class="main__table-modifiers">
+                          <div>
+                              <span class="material-symbols-outlined">
+                                  check
+                              </span>
+                          </div>
+  
+                          <div>
+                              <span class="material-symbols-outlined">
+                                  attach_file
+                              </span>
+                          </div>
+  
+                          <div>
+                              <span class="material-symbols-outlined table-icons--center">
+                                  delete
+                              </span>
+                          </div>
+  
+                          <div>
+                              <span class="material-symbols-outlined">
+                                  edit
+                              </span>
+                          </div>
+  
+                      </div>
+                  </td>
+              </tr>
+                      `;
+      }
+      prioritariosElements.querySelector(".main__table").innerHTML = body;
+    };
+    
+  }
 
-        // POST Method (Priority register)
-        fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8"
-            },
-            body: ''
-        })
-
-
-
-
-
-
-        const mostrarData = (data) => {
-            let body = `
-            <tr>
-                <th>Nº</th>
-                <th>Fecha de Entrada</th>
-                <th>Area responsable</th>
-                <th>Localidad</th>
-                <th>Solicitante</th>
-                <th>Estado</th>
-                <th>Modificar</th>
-            </tr>
-            `
-            for (let i = 0; i < data.length; i++) {
-                body += `
-             <tr>
-                <td>${data[i].nota}</td>
-                <td>${data[i].fecha}</td>
-                <td>${data[i].areaResponsable}</td>
-                <td>${data[i].localidad}</td>
-                <td>${data[i].solicitanteForm}</td>
-                <td>${data[i].estadoForm}</td>
-            </tr> `
-            }
-        }
-
-        // GET Method (Priorities Registers)
-        fetch(url)
-            .then(res => res.json())
-            .then(registros => mostrarData(registros))
-            .catch(err => console.log(err))
-
-    }
-
-
-    return prioritariosElements
-}
+  return prioritariosElements;
+};
